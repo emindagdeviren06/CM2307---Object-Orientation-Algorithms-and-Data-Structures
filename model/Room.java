b157bd5c2867442ae6b102f91086b54d86ff5528
Emin Dagdeviren <emind060108@gmail.com>
2026-01-05 07:50:20 +0000

    Adding room class that will allow users to view and interact with the rooms

---
+++ model/Room.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Room {
+    private String location;
+    private double size; //size will be in square meters
+    private String amenities; //amenities such as wifi, the kitchen etc...
+    private double rent;
+
+
+public Room(String Location, double size, String amenities, double rent) {
+    this.location = location;
+    this.size = size;
+    this.amenities = amenities;
+    this.rent = rent;
+}
+
+public String getLocation() {
+    return location;
+}
+
+public double getSize() {
+    return size;
+}
+
+public String getAmenities() {
+    return amenities;
+}
+
+public double getRent() {
+    return rent;
+}
+
+
+//description of the room and it's description
+@Override
+public String toString() {
+    return "Room at" + location + "| Size:" + size + "sqm | Amenities:" + amenities + " | Rent:£" + rent;
+}
+
+}
