77ed90a5be7ed3eac7c94dc03e2059d37c7e1620
Emin Dagdeviren <emind060108@gmail.com>
2026-01-05 06:42:47 +0000

    Adding a user class for the initial stage

---
+++ model/User.java
@@ -0,0 +1,27 @@
+package model;
+
+public abstract class User { 
+    protected String userId;
+    protected String name;
+    protected String email;
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public abstract void login();
+
+}
